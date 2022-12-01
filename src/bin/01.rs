7b96f8f5a275df435d681ab39cd49e80c12d271e
ElioStalteri <30959785+ElioStalteri@users.noreply.github.com>
2022-12-01 09:24:17 +0000

    first day completed

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,30 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i32> {
+
+    let elfs = input.split("\n\n");
+
+    let mut max_calories = Some(0);
+    for elf in elfs {
+        let calories = elf.split('\n').map(|food| food.parse::<i32>().unwrap_or(0)).reduce(|a, b| a + b);
+        if calories.is_some() && calories > max_calories {
+            max_calories = calories;
+        }
+    }
+    max_calories
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    let elfs = input.split("\n\n");
+
+    let mut calories_arr:Vec<i32> = vec!();
+    for elf in elfs {
+        let calories = elf.split('\n').map(|food| food.parse::<i32>().unwrap_or(0)).reduce(|a, b| a + b);
+        if calories.is_some(){
+            calories_arr.push(calories.unwrap_or(0))
+        }
+    }
+    calories_arr.sort_by(|a, b| b.cmp(a));
+    let result:i32 = calories_arr[0..3].iter().sum();
+    Some(result)
 }
 
 fn main() {
@@ -19,12 +40,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = aoc::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
     fn test_part_two() {
         let input = aoc::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }

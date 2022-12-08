ef450c12c5394480091e00adc5f8d24cf9d10fb6
elio-stalteri <61432158+elio-stalteri@users.noreply.github.com>
2022-12-08 11:35:37 +0000

    almost completed day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -51,8 +51,6 @@ pub fn part_one(input: &str) -> Option<i32> {
         })
         .collect();
 
-    dbg!(visible_from_right.clone());
-
     let mut max: Vec<i32> = vec![-1; trees[0].len()];
     let visible_from_top: Vec<Vec<i32>> = trees
         .iter()
@@ -95,8 +93,8 @@ pub fn part_one(input: &str) -> Option<i32> {
         tmp
     };
 
-    let mut count_visibles_coord: Vec<Vec<usize>> = vec![];  
-    let mut count_visibles = 0 ;
+    let mut count_visibles_coord: Vec<Vec<usize>> = vec![];
+    let mut count_visibles = 0;
     for (ir, row) in trees.iter().enumerate() {
         for (ic, v) in row.iter().enumerate() {
             if ic != 0
@@ -108,16 +106,162 @@ pub fn part_one(input: &str) -> Option<i32> {
                     || visible_from_top[ir][ic] > -1
                     || visible_from_bottom[ir][ic] > -1)
             {
-                count_visibles +=1;
-                count_visibles_coord.push(vec![ir,ic]);
+                count_visibles += 1;
+                count_visibles_coord.push(vec![ir, ic]);
             }
         }
     }
-    dbg!(count_visibles_coord);
+
     Some(count_visibles + outside_trees)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let trees: Vec<Vec<i32>> = input
+        .split('\n')
+        .map(|v| {
+            v.chars()
+                .map(|v| v.to_string().parse::<i32>().ok().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let mut max: Vec<i32> = vec![];
+    let visible_from_left: Vec<Vec<i32>> = trees
+        .iter()
+        .map(|row| {
+            max = vec![];
+            row.iter()
+                .map(|v| {
+                    if max.is_empty() {
+                        max.push(*v);
+                        return 0;
+                    }
+                    let mut count = 0;
+                    for b in max.iter().rev() {
+                        count += 1;
+                        if b >= v {
+                            break;
+                        }
+                    }
+                    max.push(*v);
+                    count
+                })
+                .collect()
+        })
+        .collect();
+
+    max = vec![];
+    let visible_from_right: Vec<Vec<i32>> = trees
+        .iter()
+        .map(|r| {
+            max = vec![];
+            let mut row = r.clone();
+            row.reverse();
+            row = row
+                .iter()
+                .map(|v| {
+                    if max.is_empty() {
+                        max.push(*v);
+                        return 0;
+                    }
+                    let mut count = 0;
+                    for b in max.iter().rev() {
+                        count += 1;
+                        if b >= v {
+                            break;
+                        }
+                    }
+                    max.push(*v);
+                    count
+                })
+                .collect();
+            row.reverse();
+            row
+        })
+        .collect();
+
+
+    let mut max: Vec<Vec<i32>> = vec![vec![]; trees[0].len()];
+    let visible_from_top: Vec<Vec<i32>> = trees
+        .iter()
+        .map(|row| {
+            row.iter()
+                .enumerate()
+                .map(|(i, v)| {
+                    if max[i].is_empty() {
+                        max[i].push(*v);
+                        return 0;
+                    }
+                    let mut count = 0;
+                    for b in max[i].iter().rev() {
+                        count += 1;
+                        if b >= v {
+                            break;
+                        }
+                    }
+                    max[i].push(*v);
+                    count
+                })
+                .collect()
+        })
+        .collect();
+
+    
+
+    let mut max: Vec<Vec<i32>> = vec![vec![]; trees[0].len()];
+    let visible_from_bottom: Vec<Vec<i32>> = {
+        let mut tmp: Vec<Vec<i32>> = trees.clone();
+        tmp.reverse();
+        tmp = tmp
+            .iter()
+            .map(|row| {
+                // let mut row = r.clone();
+                // row.reverse();
+                row.iter()
+                    .enumerate()
+                    .map(|(i, v)| {
+                        if max[i].is_empty() {
+                            max[i].push(*v);
+                            return 0;
+                        }
+                        let mut count = 0;
+                        for b in max[i].iter().rev() {
+                            count += 1;
+                            if b >= v {
+                                break;
+                            }
+                        }
+                        max[i].push(*v);
+                        count
+                    })
+                    .collect()
+            })
+            .collect();
+        tmp.reverse();
+        tmp
+    };
+
+
+
+    // let mut count_visibles_coord: Vec<Vec<usize>> = vec![];
+    // let mut count_visibles = 0;
+    // for (ir, row) in trees.iter().enumerate() {
+    //     for (ic, v) in row.iter().enumerate() {
+    //         if ic != 0
+    //             && ic != row.len() - 1
+    //             && ir != 0
+    //             && ir != trees.len() - 1
+    //             && (visible_from_right[ir][ic] > -1
+    //                 || visible_from_left[ir][ic] > -1
+    //                 || visible_from_top[ir][ic] > -1
+    //                 || visible_from_bottom[ir][ic] > -1)
+    //         {
+    //             count_visibles += 1;
+    //             count_visibles_coord.push(vec![ir, ic]);
+    //         }
+    //     }
+    // }
+
     None
 }
 
